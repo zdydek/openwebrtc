58b7c133d513341908ff7444ff47a287c7be4ef0
Philippe Normand <philn@igalia.com>
2016-08-04 10:38:51 +0200

    owr_transport: plug payload leaks

--- transport/owr_transport_agent.c
+++ transport/owr_transport_agent.c
@@ -1351,6 +1351,7 @@ static gboolean emit_bitrate_change(GHashTable *args)
             GST_INFO("Updating bitrate to %u from %u", bitrate, old_bitrate);
             g_object_set(payload, "bitrate", bitrate, NULL);
         }
+        g_object_unref(payload);
     } else
         GST_WARNING("No send payload set for media session");
 
@@ -2736,8 +2737,10 @@ static GstElement * on_rtpbin_request_aux_sender(G_GNUC_UNUSED GstElement *rtpbi
         goto no_retransmission;
 
     g_object_get(payload, "rtx-payload-type", &rtx_pt, NULL);
-    if (rtx_pt < 0)
+    if (rtx_pt < 0) {
+        g_object_unref(payload);
         goto no_retransmission;
+    }
 
     rtxsend = gst_element_factory_make("rtprtxsend", NULL);
     g_return_val_if_fail(rtxsend, NULL);
@@ -2757,6 +2760,7 @@ static GstElement * on_rtpbin_request_aux_sender(G_GNUC_UNUSED GstElement *rtpbi
     if (rtx_time)
         g_object_set(rtxsend, "max-size-time", rtx_time, NULL);
 
+    g_object_unref(payload);
     return create_aux_bin("rtprtxsend", rtxsend, session_id);
 
 no_retransmission:
@@ -4361,6 +4365,7 @@ static gboolean on_payload_adaptation_request(GstElement *screamqueue, guint pt,
     payload = _owr_media_session_get_send_payload(media_session);
     g_assert(pt);
     g_object_get(payload, "rtx-payload-type", &pt_rtx, "adaptation", &adapt_type, NULL);
+    g_object_unref(payload);
     /* Use adaptation for this payload if not retransmission */
     return (adapt_type == OWR_ADAPTATION_TYPE_SCREAM) && (pt != pt_rtx);
 }
